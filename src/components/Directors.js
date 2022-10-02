3e37b96f16441a8d0eac6e745cda74e04c435e76
ezra.kipchumba@student.moringaschool.com <ezra.kipchumba@student.moringaschool.com>
2022-10-02 10:42:17 +0300

    complete

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  // { directors.map((director, index) =>return <ol key={index}>{director}</ol>)}
+  return (
+    <div>
+      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((director, index) => {
+        return (
+          <div key={index}>
+            <h1>{director.name}</h1>
+            <ul>
+              {director.movies.map((movie, index) => {
+                return <li key={index}>{movie}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Directors;
